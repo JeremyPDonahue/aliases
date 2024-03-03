14cf8fee0c17638bed73ec93084b8a957f111f02
Jeremy Donahue <jeremypdonahue@gmail.com>
2024-03-03 19:12:24 +0000

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # aliases
+
+Shell aliases to simplify CLI tasks
+
+These are my personal aliases that I like to have on hand to help increase my productivity.
+Storing them here so I can easily access them from whatever machine I am on.
+
+If you find them useful too, download them and add them to your rc file. (~./bashrc, ~/.zshrc, etc.) 
---
+++ aliases.sh
@@ -0,0 +1,31 @@
+# General Use
+alias s="sudo"
+alias c="clear"
+alias srm="sudo rm"
+
+# Shell
+alias ozh="vi ~/.bashrc"
+alias szh="source ~/.bashrc"
+alias install="sudo apt install"
+
+# Service Troubleshooting
+alias status="sudo systemctl status"
+alias bounce="sudo systemctl restart"
+alias logs="sudo journalctl -u"
+
+# Network Troubleshooting
+alias ip="ip addr | grep inet"
+
+# Kubernetes
+alias kc="kubectl"
+alias micro="sudo microk8s"
+
+# Docker
+alias docker="sudo docker"
+
+# Git
+alias gs="git status"
+alias ga="git add"
+alias gc="git commit"
+alias gl="git logs"
+
