1712a797c63aeff9b9df1ff8de0e19ae81fa8863
Jeremy Donahue <jeremypdonahue@gmail.com>
2024-03-03 21:07:25 +0000

    Adding more aliases

--- aliases.sh
+++ aliases.sh
@@ -1,31 +1,45 @@
 # General Use
 alias s="sudo"
 alias c="clear"
+alias last="cd ~-"
 alias srm="sudo rm"
+alias coffee="caffeinate -d"
 
 # Shell
+alias ep="echo $PATH"
 alias ozh="vi ~/.bashrc"
 alias szh="source ~/.bashrc"
 alias install="sudo apt install"
 
 # Service Troubleshooting
+alias logs="sudo journalctl -u"
 alias status="sudo systemctl status"
 alias bounce="sudo systemctl restart"
-alias logs="sudo journalctl -u"
 
 # Network Troubleshooting
 alias ip="ip addr | grep inet"
 
 # Kubernetes
-alias kc="kubectl"
+alias k="kubectl"
 alias micro="sudo microk8s"
+alias kgp="kubectl get pods"
+alias kdp="kubectl describe pod"
+alias kdd="kubectl delete deployment"
 
 # Docker
-alias docker="sudo docker"
+alias d="sudo docker"
+alias dimg="docker images"
+alias dpush="docker push"
+alias drm="docker rm"
+alias dpsa="docker ps -a"
 
 # Git
 alias gs="git status"
 alias ga="git add"
 alias gc="git commit"
 alias gl="git logs"
+alias pull="git pull"
+alias push="git push"
 
+# Python
+alias p3="python3"
